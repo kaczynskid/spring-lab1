3b98e65abc43d6b82e0c5be65a250ae30bfb9f90
Darek Kaczyński <kaczynskid@gmail.com>
2016-11-28 12:20:13 +0100

    Dependency Injection

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -1,7 +1,11 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,16 +20,41 @@ public class DemoApplication {
 @RestController
 class Hello {
 
+	private final Greeting greeting;
+
+	public Hello(Greeting greeting) {
+		this.greeting = greeting;
+	}
+
 	@GetMapping("/hello")
 	public Greeting greet() {
-		return new Greeting("Hello world");
+		return greeting;
 	}
 
 }
 
+@RestController
+class Hello2 {
+
+	private final Greeting greeting;
+
+	public Hello2(@Qualifier("defaultGreeting") Greeting greeting) {
+		this.greeting = greeting;
+	}
+
+	@GetMapping("/hello2")
+	public Greeting greet() {
+		return greeting;
+	}
+
+}
+
+@Component("defaultGreeting")
 class Greeting {
 
-	public String message;
+	public String message = "Hello world!";
+
+	public Greeting() { }
 
 	public Greeting(String message) {
 		this.message = message;

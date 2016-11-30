c435ae639399210c6a392488415a15aa866b18cd
Darek Kaczyński <kaczynskid@gmail.com>
2016-11-30 13:36:19 +0100

    Added metrics and graphite reporting

--- reservation-service/src/main/java/com/example/ReservationsRepository.java
+++ reservation-service/src/main/java/com/example/ReservationsRepository.java
@@ -7,14 +7,21 @@ import java.util.Optional;
 
 import com.querydsl.core.types.Predicate;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.actuate.metrics.CounterService;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.HandleAfterCreate;
+import org.springframework.data.rest.core.annotation.HandleAfterDelete;
+import org.springframework.data.rest.core.annotation.HandleAfterSave;
+import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.stereotype.Component;
 
 @RepositoryRestResource
-interface ReservationsRepository extends JpaRepository<Reservation, Long>,
+public interface ReservationsRepository extends JpaRepository<Reservation, Long>,
 		QueryDslPredicateExecutor<Reservation>, LegacyReservationsRepository {
 
 	class Spec {
@@ -40,4 +47,36 @@ interface ReservationsRepository extends JpaRepository<Reservation, Long>,
 	@Override
 	@RestResource(exported = false)
 	void delete(Long id);
+}
+
+@Slf4j
+@Component
+@RepositoryEventHandler
+class ReservationEventHandler {
+
+	private final CounterService counter;
+
+	public ReservationEventHandler(CounterService counter) {
+		this.counter = counter;
+	}
+
+	@HandleAfterCreate
+	public void create(Reservation reservation) {
+		log.info("Created reservation for {}.", reservation.getName());
+		counter.increment("count");
+		counter.increment("create");
+	}
+
+	@HandleAfterSave
+	public void save(Reservation reservation) {
+		log.info("Updated reservation for {}.", reservation.getName());
+		counter.increment("save");
+	}
+
+	@HandleAfterDelete
+	public void delete(Reservation reservation) {
+		log.info("Removed reservation for {}.", reservation.getName());
+		counter.decrement("count");
+		counter.increment("delete");
+	}
 }
